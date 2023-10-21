371f403273e937220fcaca2dbf11a1ad04dccd76
Akuanaksehatt <111202013075@mhs.dinus.id>
2023-10-21 15:19:08 +0700

    add enforeach

--- resources/views/pages/stocks/index.blade.php
+++ resources/views/pages/stocks/index.blade.php
@@ -97,7 +97,6 @@
                 </div>
             </div>
         </div>
-
         @include('layouts.footers.auth.footer')
     </div>
 @endsection
--- resources/views/pages/transaction/index.blade.php
+++ resources/views/pages/transaction/index.blade.php
@@ -119,6 +119,7 @@
                                                 @endif
                                         </td>
                                     </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
