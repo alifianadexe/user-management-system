57a169e5f140168f25dc108e702f25ca73d518be
Dimaspadma <Dimaspadma@users.noreply.github.com>
2023-10-26 23:16:01 +0700

    Add DataTable Jquery (+) Add Jquary (/) index all page table id

--- resources/views/pages/resource/index.blade.php
+++ resources/views/pages/resource/index.blade.php
@@ -12,8 +12,8 @@
                 <div class="card-header pb-0">
                 </div>
                 <div class="card-body px-2 pt-2 pb-5">
-                    <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
+                    <div class="table-responsive p-3">
+                        <table id="myTable" class="table align-items-center mb-0">
                             <thead>
                                 <tr>
                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Kingdom
--- resources/views/pages/transaction/index.blade.php
+++ resources/views/pages/transaction/index.blade.php
@@ -14,8 +14,8 @@
                     </div>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
-                    <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
+                    <div class="table-responsive p-3">
+                        <table id="myTable" class="table align-items-center mb-0">
                             <thead>
                                 <tr>
                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name
